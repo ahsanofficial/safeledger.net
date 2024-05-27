8a63b0ee785221522a5a4bca4df394bf18edbecc
fareedShopyGram <farid@myshopygram.com>
2024-05-27 16:00:31 +0530

    Update

--- components/Nav/page.tsx
+++ components/Nav/page.tsx
@@ -141,13 +141,13 @@ export default function Header() {
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end items-center	">
           <ForumIcon className={`${style.fz35} text-blue-400`} />
-          <Link href="#" className="text-sm font-semibold leading-6 text-gray-900 text-blue-400 pr-2">
+          <Link href="#" className="ml-2 text-sm font-semibold leading-6 text-gray-900 text-blue-400 pr-2">
             Get Consultation  <br /><a >+44 20 3318 1326</a>
           </Link>
           <Listbox value={selected} onChange={setSelected}>
             {({ open }) => (
               <>
-                <div className="relative mt-2">
+                <div className="relative">
                   <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
                     <span className="flex items-center">
                       <img src={selected?.avatar} alt="" className="h-5 w-5 flex-shrink-0 rounded-full" />
