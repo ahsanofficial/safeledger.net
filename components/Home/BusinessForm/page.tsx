2ed8031adfc321d905a87b735eea3ecfb6324f14
fareedShopyGram <farid@myshopygram.com>
2024-05-28 14:46:41 +0530

    Fix Error

--- components/Home/BusinessForm/page.tsx
+++ components/Home/BusinessForm/page.tsx
@@ -5,13 +5,11 @@ import { Button, Grid, TextField } from '@mui/material';
 import ZohoFormService from '@/components/Form/ZohoForm';
 
 const BusinessForm = () => {
-
-
     return (
-        <div className={`flex ${styles.bg} grid sm:grid-cols-2 lg:grid-cols-2 items-center	`}>
+        <div className={`flex ${styles.bg} grid sm:grid-cols-2 lg:grid-cols-2 items-center`}>
             <div className={`${styles.top} sm:mt-0 lg:mt-0 mt-6 sm:ml-0 lg:ml-0 ml-4`}>
                 <h2 className="sm:text-4xl text-blue-600 font-extrabold text-3xl w-3/5">Start your Business Today</h2>
-                <p className='sm:pr-6 mg:pr-6 mt-'>Speak with a SafeLedger specialist today to ensure compliance, efficiency, and growth as you navigate every stage of your company's journey. Together, let's turn your business goals into reality. </p>
+                <p className='sm:pr-6 mg:pr-6 mt-'>Speak with a SafeLedger specialist today to ensure compliance, efficiency, and growth as you navigate every stage of your company&apos;s journey. Together, let&apos;s turn your business goals into reality. </p>
             </div>
             <div>
                 {/* <form noValidate autoComplete="off">
@@ -23,7 +21,7 @@ const BusinessForm = () => {
                                 label="Name"
                                 variant="outlined"
                                 fullWidth
-                                className='rounded-3xl	'
+                                className='rounded-3xl'
                             />
                         </Grid>
                         <Grid item xs={6}>
@@ -45,10 +43,10 @@ const BusinessForm = () => {
                             />
                         </Grid>
                     </Grid>
-                    <Button variant="contained" color="primary" className='mt-6 bg-indigo-700 text-white rounded-full	' fullWidth>
+                    <Button variant="contained" color="primary" className='mt-6 bg-indigo-700 text-white rounded-full' fullWidth>
                         Submit
                     </Button>
-                    <span className='text-xs text-slate-500	'>By clicking, you agree to our Terms & Conditions , Privacy and Data Protection Policy</span>
+                    <span className='text-xs text-slate-500'>By clicking, you agree to our Terms & Conditions , Privacy and Data Protection Policy</span>
                 </form> */}
                 <div className='pt-4 bg-indigo-700 mb-8 sm:mt-0 lg:mt-0 mt-8'>
                     <ZohoFormService service={''} />
